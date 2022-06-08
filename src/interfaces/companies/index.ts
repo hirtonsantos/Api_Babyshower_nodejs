6ef1a825ac956c3edcd115286f8c390babc60adc
Pedro Basilio <pedronobasi@gmail.com>
2022-06-08 10:00:18 -0300

    feature: intefaces ICompany | ICompanyLogin

--- src/interfaces/companies/index.ts
+++ src/interfaces/companies/index.ts
@@ -1,4 +1,25 @@
-export interface ICompanieRegister {
+export interface ICompany {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  razaoSocial: string;
+  cnpj: string;
+  phone?: string;
+  imageLogo?: string;
+}
+
+export interface ICompanyLogin {
+  username: string;
+  email: string;
+  password: string;
+  razaoSocial: string;
+  cnpj: string;
+  phone?: string;
+  imageLogo?: string;
+}
+
+export interface ICompanyRegister {
   username: string;
   email: string;
   password: string;
