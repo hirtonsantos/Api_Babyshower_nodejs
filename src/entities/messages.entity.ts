209d13b7c110a5b5b5d314dfac20f820aba9f83c
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-06-12 23:17:23 -0300

    feat(chat): create tables chat and messages

---
+++ src/entities/messages.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryColumn, ManyToOne } from "typeorm";
+import { v4 as uuid } from "uuid"
+import { Chat } from "./chat.entity";
+
+@Entity("message", {
+    orderBy: {
+        createdAt: "DESC",
+    },
+})
+export class Message {
+
+    @PrimaryColumn("uuid")
+    readonly id: string
+
+    @Column()
+    message: string
+
+    @Column("boolean", {default: false})
+    read_message: boolean
+
+    @Column()
+    createdAt: Date
+
+    @Column()
+    parent_id: number
+
+    @ManyToOne(() => Chat, (chat) => chat.id)
+    chat: Chat
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+    }
+
+}
