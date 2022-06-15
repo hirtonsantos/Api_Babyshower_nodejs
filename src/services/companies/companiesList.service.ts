86b3eeaf569f825bafd999d7e2a45ea8917c7a0b
Pedro Basilio <pedronobasi@gmail.com>
2022-06-14 21:32:35 -0300

    feat: passing in all tests

--- src/services/companies/companiesList.service.ts
+++ src/services/companies/companiesList.service.ts
@@ -1,9 +1,14 @@
-const companiesListService = () => {
-    try {
-        
-    } catch (error) {
-        
-    }
+import { AppDataSource } from "../../data-source"
+import { Company } from "../../entities/companies.entity"
+
+const companiesListService = async () => {    
+    
+    const companiesRepository = AppDataSource.getRepository(Company)
+
+    const companies = companiesRepository.find()
+
+    return companies
+
 }
 
 export default companiesListService
