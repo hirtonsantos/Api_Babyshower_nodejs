b0a19fd339afda414a8363a2b1e783e673df9177
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-06-16 12:00:46 -0300

    feat[upload]: verifying idHash in file

--- src/services/upload/deleteImage.service.ts
+++ src/services/upload/deleteImage.service.ts
@@ -4,6 +4,9 @@ import { promisify } from "util"
 import fs from "fs"
 import path from "path"
 import { compare } from "bcrypt"
+import { Administrator } from "../../entities/administrators.entity"
+import { AppDataSource } from "../../data-source"
+import { DeleteObjectRequest } from "aws-sdk/clients/s3"
 
 export const deleteImageService = async (url: string, idUser: string) => {
     //regex uuid
@@ -13,21 +16,24 @@ export const deleteImageService = async (url: string, idUser: string) => {
     if(firstIndex === -1) throw new AppError(404, {Message: "file not found"})
 
     const key = url.substring(firstIndex)
-
     const idUserHash =  key.replace(re, "").replace(reEnd, "").substring(1)
-    console.log(idUserHash)
 
-    if(!await compare(idUser, idUserHash)) throw new AppError(401, {Error: "You can't access information of another user"})
+    const admRepository = AppDataSource.getRepository(Administrator) 
+    const admLogged = await admRepository.findBy({id: idUser})
+
+    if(!admLogged && !await compare(idUser, idUserHash)){
+        throw new AppError(401, {Error: "You can't access information of another user"})
+    }
 
     if(process.env.STORAGE_TYPE === "s3"){
         const s3 = new AWS.S3()
 
-        /* s3.deleteObject(
-            {Bucket: process.env.BUCKET_NAME,
-            Key: key}
-        )
+        s3.deleteObject({
+            Bucket: process.env.BUCKET_NAME as string,
+            Key: key
+        } as DeleteObjectRequest)
         .promise()
-        .then(res => console.log(res)) */
+        .then(res => console.log(res))
 
     } else {
         const filePath = path.resolve(__dirname,"..","..","..","tmp","uploads",key)
