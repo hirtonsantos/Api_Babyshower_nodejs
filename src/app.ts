f880a3b026bb166f88cfdf976aec45e9cda1dd59
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-06-20 17:19:36 -0300

    doc[swagger]: add companies documentation

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,25 @@ import express from "express";
 import "express-async-errors";
 import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
-import cors from "cors"
+import cors from "cors";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
-app.use(cors({
-    origin: '*' //incluir vercel do front aqui e api flask
-}))
+app.use(
+  cors({
+    origin: "*", //incluir vercel do front aqui e api flask
+  })
+);
 
 app.use(express.json());
+app.use(
+  "/api-documentation",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
+
 appRoutes(app);
 
 app.use(errorMiddleware);
