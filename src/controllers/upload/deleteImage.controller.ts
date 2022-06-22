fa080814819eb0e63f65b1796e12c338ea6ac7ac
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-06-21 23:08:35 -0300

    feat[route]: modify to put route

--- src/controllers/upload/deleteImage.controller.ts
+++ src/controllers/upload/deleteImage.controller.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from "express"
-import { deleteImageService } from "../../services/upload/deleteImage.service"
+import { Request, Response } from "express";
+import { deleteImageService } from "../../services/upload/deleteImage.service";
 
 export const deleteImageController = async (req: Request, res: Response) => {
-    const { key } = req.body
+  const { key } = req.body;
 
-    const response = await deleteImageService(key, req.decoded.id as string)
+  const response = await deleteImageService(key, req.decoded.id as string);
 
-    if(response){
-        return res.status(204).json()
-    }
+  if (response) {
+    return res.status(204).json();
+  }
 
-    return res.json({Error: "Não deletado"})
-}
+  return res.json({ Error: "Não deletado" });
+};
